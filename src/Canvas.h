898ab4d68a0c9f1fc3f9c0386066b2c7243137ef
成田 輝久 <hiizoka@gmail.com>
2014-11-03 14:29:32 +0900

    複数タイル同時選択実装

--- src/Canvas.h
+++ src/Canvas.h
@@ -25,7 +25,9 @@ struct BrushInfo {
 
 struct CanvasInfo {
   ofVec2f tileSize;
-  ofVec2f selectedTile;
+//  ofVec2f selectedTile;
+  vector<ofVec2f> selectedTiles;
+  ofVec2f   selectedTileSize;
   ofTexture image;
   int       layer;
   BrushInfo brush;
@@ -42,7 +44,7 @@ public:
     addWidget(ofPtr<Segment>(new Segment(ofRectangle(10,0,84,28), 3, info->layer)));
     // brush
     addWidget(ofPtr<Label>( new Label(ofRectangle(100,0,90,28), font, "size")));
-    addWidget(ofPtr<Slider>(new Slider(ofRectangle(150,0,90,28), 1,1,10, &info->brush.size, 1)));
+    addWidget(ofPtr<Slider>(new Slider(ofRectangle(150,0,90,28), 1,1,30, &info->brush.size, 1)));
     addWidget(ofPtr<RefLabel<float> >(new RefLabel<float>(ofRectangle(250,0,40,28), font,info->brush.size)));
     addWidget(ofPtr<Label>( new Label(ofRectangle(310,0,30,28), font, "amt")));
     addWidget(ofPtr<Slider>(new Slider(ofRectangle(340,0,90,28), 1.0,0.01,1.0, &info->brush.amt, 0.01)));
@@ -63,11 +65,15 @@ public:
     this->info = info;
     setContentSize(ofVec2f(info->image.getWidth(), info->image.getHeight()));
     hoveredTile = ofVec2f(0,0);
-    info->selectedTile = ofVec2f(0,0);
+    info->selectedTiles.clear();
+    info->selectedTiles.push_back(ofVec2f(0,0));
+    info->selectedTileSize = ofVec2f(0,0);
     
     info->brush.shape = RECT;
     info->brush.size  = 3;
     info->brush.amt   = 0.5;
+    dragging = false;
+    dragStartTile = ofVec2f(-1,-1);
   }
 
   virtual void drawImpl() {
@@ -88,27 +94,41 @@ public:
            info->tileSize.x,info->tileSize.y);
     
     // selected tile
-    ofSetColor(0);
-    ofSetLineWidth(4);
-    ofNoFill();
-    ofRect(info->selectedTile.x * info->tileSize.x, info->selectedTile.y * info->tileSize.y,
-           info->tileSize.x,info->tileSize.y);
-    ofSetColor(255);
-    ofSetLineWidth(2);
-    ofRect(info->selectedTile.x * info->tileSize.x, info->selectedTile.y * info->tileSize.y,
-           info->tileSize.x,info->tileSize.y);
-    ofEnableSmoothing();
+    int tileIdx = 0;
+    for(int ty=0;ty<info->selectedTileSize.y;ty++) {
+      for(int tx=0;tx<info->selectedTileSize.x;tx++) {
+        ofSetColor(255);
+        ofSetLineWidth(2);
+        ofRect(info->selectedTiles[tileIdx].x * info->tileSize.x, info->selectedTiles[tileIdx].y * info->tileSize.y,
+               info->tileSize.x,info->tileSize.y);
+        ofEnableSmoothing();
+        
+        tileIdx ++;
+      }
+    }
   }
   virtual void mouseMovedImpl(int x, int y) {
     hoveredTile = ofVec2f((int)(x / info->tileSize.x), (int)(y / info->tileSize.y));
   }
   virtual void mouseDraggedImpl(int x, int y, int button) {
-    
+    ofVec2f dTile = ofVec2f((int)(x / info->tileSize.x), (int)(y / info->tileSize.y));
+    if(dTile.x < dragStartTile.x || dTile.y < dragStartTile.y) return;
+    info->selectedTiles.clear();
+    for(int ty=dragStartTile.y;ty<=dTile.y;ty++) {
+      for(int tx=dragStartTile.x;tx<=dTile.x;tx++) {
+        info->selectedTiles.push_back(ofVec2f(tx, ty));
+      }
+    }
+    info->selectedTileSize = ofVec2f(dTile.x-dragStartTile.x+1,dTile.y-dragStartTile.y+1);
   }
   virtual void mousePressedImpl(int x, int y, int button) {
-    info->selectedTile = ofVec2f((int)(x / info->tileSize.x), (int)(y / info->tileSize.y));
+    info->selectedTiles.clear();
+    dragStartTile = ofVec2f((int)(x / info->tileSize.x), (int)(y / info->tileSize.y));
+    info->selectedTiles.push_back(ofVec2f((int)(x / info->tileSize.x), (int)(y / info->tileSize.y)));
+    info->selectedTileSize = ofVec2f(1,1);
   }
   virtual void mouseReleasedImpl(int x, int y, int button) {
+    
   }
   virtual void mouseScrolledImpl(int mx, int my, int sx, int sy) {
     hoveredTile = ofVec2f((int)(mx / info->tileSize.x), (int)(my / info->tileSize.y));
@@ -116,6 +136,8 @@ public:
   private:
   CanvasInfo *info;
   ofVec2f hoveredTile;
+  bool dragging;
+  ofVec2f dragStartTile;
 };
 
 class CanvasFrame : public ofxFrame {
@@ -157,7 +179,6 @@ public:
     }
     
     // hovered tile
-    
     ofFill();
     if(info->erase) {
       ofSetColor(0,0,0,122);
@@ -165,22 +186,28 @@ public:
              info->tileSize.x * info->brush.size,info->tileSize.y * info->brush.size);
     } else {
       ofSetColor(255,255,255,122);
-      for(int x=0;x<info->brush.size;x++) {
-        for(int y=0;y<info->brush.size;y++) {
+      int tIdxX = 0, tIdxY = 0;
+      for(int y=0;y<info->brush.size * info->selectedTileSize.y;y++) {
+        for(int x=0;x<info->brush.size * info->selectedTileSize.x;x++) {
           info->image.drawSubsection((hoveredTile.x + x) * info->tileSize.x, (hoveredTile.y + y) * info->tileSize.y, info->tileSize.x, info->tileSize.y,
-                                 info->selectedTile.x*info->tileSize.x, info->selectedTile.y*info->tileSize.y);
+          info->selectedTiles[tIdxX + tIdxY * info->selectedTileSize.x].x*info->tileSize.x, info->selectedTiles[tIdxX + tIdxY * info->selectedTileSize.x].y*info->tileSize.y);
+          tIdxX ++;
+          if(tIdxX >= info->selectedTileSize.x)tIdxX = 0;
         }
+        tIdxX = 0;
+        tIdxY ++;
+        if(tIdxY >= info->selectedTileSize.y)tIdxY = 0;
       }
     }
     ofSetColor(255);
     ofSetLineWidth(4);
     ofNoFill();
     ofRect(hoveredTile.x * info->tileSize.x, hoveredTile.y * info->tileSize.y,
-           info->tileSize.x * info->brush.size,info->tileSize.y * info->brush.size);
+           info->tileSize.x * info->brush.size* info->selectedTileSize.x,info->tileSize.y * info->brush.size* info->selectedTileSize.y);
     ofSetColor(255);
     ofSetLineWidth(2);
     ofRect(hoveredTile.x * info->tileSize.x, hoveredTile.y * info->tileSize.y,
-           info->tileSize.x * info->brush.size,info->tileSize.y * info->brush.size);
+           info->tileSize.x * info->brush.size* info->selectedTileSize.x,info->tileSize.y * info->brush.size* info->selectedTileSize.y);
     ofEnableSmoothing();
   }
   ofVec2f getTileIdx(int x, int y) {
@@ -194,27 +221,38 @@ public:
   }
   void pickTile(ofVec2f tileIdx) {
     const ofVec2f &tile = tiles[info->layer][tileIdx.y*(contentSize.x/info->tileSize.x)+tileIdx.x];
-    if(tile.x < 0)return;
-    info->selectedTile = tile;
+    if(tile.x < 0)info->selectedTiles[0] = ofVec2f(-1,-1);
+    info->selectedTiles[0] = tile;
   }
-  void fillRectTile(ofRectangle rect, ofVec2f tile, float amt=1.0) {
+  void fillRectTile(ofRectangle rect, const vector<ofVec2f> &tile, ofVec2f size, float amt=1.0) {
+    int tIdxX = 0, tIdxY = 0;
     for(int y=rect.getTop();y <= rect.getBottom();y++) {
       if(y < 0 || y >= contentSize.y/info->tileSize.y)continue;
       for(int x=rect.getLeft();x <= rect.getRight();x++) {
         if(x < 0 || x >= contentSize.x/info->tileSize.x)continue;
         if(ofRandom(1.0) <= amt) {
-          putTile(ofVec2f(x, y), tile);
+          putTile(ofVec2f(x, y), tile[tIdxX + tIdxY * info->selectedTileSize.x]);
+          tIdxX ++;
+          if(tIdxX >= info->selectedTileSize.x)tIdxX = 0;
         }
       }
+      tIdxX = 0;
+      tIdxY ++;
+      if(tIdxY >= info->selectedTileSize.y)tIdxY = 0;
     }
   }
   
   void brushTile(int x, int y) {
     ofVec2f tileIdx = getTileIdx(x, y);
-    ofVec2f brushTile = info->selectedTile;
-    if(info->erase)brushTile = ofVec2f(-1,-1);
+    vector<ofVec2f> brushTile = info->selectedTiles;
+    ofVec2f brushTileSize = info->selectedTileSize;
+    if(info->erase) {
+      brushTile.clear();
+      brushTile.push_back(ofVec2f(-1,-1));
+      brushTileSize = ofVec2f(1,1);
+    }
     if(preBrushedTileIdx == tileIdx)return;
-    fillRectTile(ofRectangle(tileIdx, tileIdx + ofVec2f(info->brush.size-1,info->brush.size-1)), brushTile, info->brush.amt);
+    fillRectTile(ofRectangle(tileIdx, tileIdx + ofVec2f(info->brush.size*info->selectedTileSize.x - 1,info->brush.size * info->selectedTileSize.y - 1)), brushTile, brushTileSize, info->brush.amt);
     
     preBrushedTileIdx = tileIdx;
   }
@@ -251,6 +289,7 @@ public:
     } else if(button == 2){
       // right click
       pickTile(getTileIdx(x, y));
+      info->selectedTileSize = ofVec2f(1,1);
     }
     
   }
